49bf0ce938f5e7e64f51e11c38409c8e0bf4d1c0
modernscape <modernscape@gmail.com>
2026-02-05 21:35:12 +0900

    FragmentsEmbed追加

---
+++ src/app/fragments/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+export default function FragmentsEmbed() {
+  return (
+    <iframe
+      src="http://btw.cloudfree.jp/pages/fragments/"
+      style={{
+        width: "100vw",
+        height: "100vh",
+        border: "none",
+      }}
+    />
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,14 @@ import dynamic from "next/dynamic"
 const Sphere = dynamic(() => import("@/projects/sphere"), {ssr: false})
 const Distortion = dynamic(() => import("@/projects/sphere_distortion"), {ssr: false})
 const TwilightEmbed = dynamic(() => import("./twilight/page"), {ssr: false})
+const FragmentsEmbed = dynamic(() => import("./fragments/page"), {ssr: false})
 
 export default function Home() {
   const pages = [
     Sphere, //
-    Sphere,
-    Distortion,
     Distortion,
     TwilightEmbed,
+    FragmentsEmbed,
   ]
 
   const [Page] = useState(() => {
