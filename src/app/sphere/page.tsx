0d491b61deecdb9865e991a6f907b5ea3d73bd7a
modernscape <modernscape@gmail.com>
2026-05-08 00:26:56 +0900

    ./src/app/sphere/page.tsx

--- src/app/sphere/page.tsx
+++ src/app/sphere/page.tsx
@@ -1,5 +1,5 @@
 // import SphereProject from "@projects/sphere"
-import SphereProject from "../../../projects/sphere"
+import SphereProject from "../../../projects/sphere/index"
 
 export default function Page() {
   return <SphereProject />
--- src/app/sphere_distortion/page.tsx
+++ src/app/sphere_distortion/page.tsx
@@ -1,5 +1,5 @@
 // import SphereProject from "@projects/sphere_distortion"
-import SphereProject from "../../../projects/sphere_distortion"
+import SphereProject from "../../../projects/sphere_distortion/index"
 
 export default function Page() {
   return <SphereProject />
