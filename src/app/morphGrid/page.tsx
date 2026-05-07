6b8e6bc1b36a3b855e75354c62ac8e9fe8282d12
modernscape <modernscape@gmail.com>
2026-05-07 23:39:29 +0900

    Mid3_morphGrid/追加

---
+++ src/app/morphGrid/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+export default function FragmentsEmbed() {
+  return (
+    <iframe
+      src="https://modernscape.github.io/Mid3_morphGrid/"
+      style={{
+        width: "100vw",
+        height: "100vh",
+        border: "none",
+      }}
+    />
+  )
+}
