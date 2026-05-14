a7077ca96e8ee6efdbcdcf6595a3a6607df03854
modernscape <modernscape@gmail.com>
2026-05-14 15:55:57 +0900

    chocolat追加

---
+++ src/app/chocolat/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+export default function FragmentsEmbed() {
+  return (
+    <iframe
+      src="https://modernscape.github.io/26-05-14_chocolat/"
+      style={{
+        width: "100vw",
+        height: "100vh",
+        border: "none",
+      }}
+    />
+  )
+}
