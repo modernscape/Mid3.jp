8ed60ed5699f683f58371463136078ccd614ffa3
modernscape <modernscape@gmail.com>
2026-05-30 15:38:56 +0900

    NG

--- src/app/chocolat/page.tsx
+++ src/app/chocolat/page.tsx
@@ -2,21 +2,13 @@
 
 export default function FragmentsEmbed() {
   return (
-    <>
-      <style jsx>{`
-        #logo {
-          display: none;
-        }
-      `}</style>
-
-      <iframe
-        src="https://modernscape.github.io/26-05-14_chocolat/"
-        style={{
-          width: "100vw",
-          height: "100vh",
-          border: "none",
-        }}
-      />
-    </>
+    <iframe
+      src="https://modernscape.github.io/26-05-14_chocolat/"
+      style={{
+        width: "100vw",
+        height: "100vh",
+        border: "none",
+      }}
+    />
   )
 }
