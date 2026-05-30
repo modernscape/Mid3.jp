0f913fb617a3f251b3b09cd5059336b5133658f1
modernscape <modernscape@gmail.com>
2026-05-30 15:42:19 +0900

    #logoを非表示 2

--- src/app/chocolat/page.tsx
+++ src/app/chocolat/page.tsx
@@ -1,6 +1,16 @@
 "use client"
+import { useEffect } from "react"
 
 export default function FragmentsEmbed() {
+  useEffect(() => {
+    // ページが表示されたらロゴを消すクラスをbodyに追加
+    document.body.classList.add("hide-logo")
+
+    // このコンポーネントがアンマウントされたら元に戻す
+    return () => {
+      document.body.classList.remove("hide-logo")
+    }
+  }, [])
   return (
     <iframe
       src="https://modernscape.github.io/26-05-14_chocolat/"
